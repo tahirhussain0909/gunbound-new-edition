ba17bbbec8fa1d7a8cb78aa41b093dc76370fe8f
tahirhussain0909 <tahirhussain5112@gmail.com>
2024-09-20 14:46:00 +0530

    Deploying both nextjs client and websocket server

--- apps/wss/src/index.ts
+++ apps/wss/src/index.ts
@@ -1,16 +1,35 @@
-import express, { Request, Response } from "express";
+require("dotenv").config();
+
 import cors from "cors";
+import WebSocket, { WebSocketServer } from "ws";
+import express, { Request, Response } from "express";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("hi luiz");
+  res.send("websocket server is healthy");
 });
 
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
+const httpServer = app.listen(port, () => {
+  console.log(`websocket server is listening on port ${port}`);
+});
+
+httpServer.on("error", (error: Error) => {
+  console.log(`Server error: ${error}`);
+});
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on("connection", (ws: WebSocket.WebSocket) => {
+  ws.on("message", (data: WebSocket.Data, isBinary: boolean) => {
+    wss.clients.forEach((client: WebSocket.WebSocket) => {
+      if (client.readyState === WebSocket.OPEN && client !== ws) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
 });
